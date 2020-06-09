8fcd310831c775d44036489302b213bf6a2e8bfa
uijin <justwaves@naver.com>
2020-06-09 23:24:04 +0900

    Real time

--- src/containers/blocks/BlocksContainer.jsx
+++ src/containers/blocks/BlocksContainer.jsx
@@ -1,9 +1,18 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect, useCallback, useRef } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { getLastBlockNumber, getBlockList } from 'redux/modules/blocks';
+import { useParams } from 'react-router-dom';
+import web3 from 'lib/web3API';
+import {
+  getLastBlockNumber,
+  getBlockList,
+  updateBlockList,
+} from 'redux/modules/blocks';
+import { hideTransactions } from 'redux/modules/ui';
 import Blocks from 'components/blocks/Blocks';
 
 const BlocksContainer = () => {
+  const [checked, setChecked] = useState(true);
+  const { id } = useParams();
   const dispatch = useDispatch();
   const {
     lastBlockNumber,
@@ -11,13 +20,16 @@ const BlocksContainer = () => {
     error,
     blocksLoading,
     lastBlockLoading,
+    realtime,
   } = useSelector(({ blocks, loading }) => ({
     lastBlockNumber: blocks.lastBlockNumber,
     blockList: blocks.blockList,
     error: blocks.error,
+    realtime: blocks.realtime,
     lastBlockLoading: loading['blocks/GET_LAST_BLOCK_NUMBER'],
     blocksLoading: loading['blocks/GET_BLOCK_LIST'],
   }));
+  const subscriptionRef = useRef();
 
   useEffect(() => {
     dispatch(getLastBlockNumber());
@@ -27,12 +39,52 @@ const BlocksContainer = () => {
     if (lastBlockNumber) {
       dispatch(
         getBlockList({
-          startBlock: lastBlockNumber - 10,
+          startBlock: lastBlockNumber - 30,
           endBlock: lastBlockNumber,
         }),
       );
     }
   }, [dispatch, lastBlockNumber]);
+
+  // Real Time 옵션 활성화
+  useEffect(() => {
+    if (checked && realtime && lastBlockNumber && !subscriptionRef.current) {
+      try {
+        const subscription = web3.eth.subscribe(
+          'newBlockHeaders',
+          async (err, newBlock) => {
+            if (err) {
+              console.log(err);
+            }
+            const newBlockNumber = newBlock.number;
+            dispatch(updateBlockList({ newBlockNumber }));
+          },
+        );
+        subscriptionRef.current = subscription;
+      } catch (err) {
+        console.log(err);
+      }
+    }
+  }, [dispatch, lastBlockNumber, realtime, checked]);
+
+  useEffect(() => {
+    if (!checked && subscriptionRef.current) {
+      subscriptionRef.current.unsubscribe(err => {
+        if (err) {
+          console.log(err);
+        }
+      });
+      subscriptionRef.current = null;
+    }
+  }, [checked]);
+
+  const closeTransactions = useCallback(() => {
+    dispatch(hideTransactions());
+  }, [dispatch]);
+
+  const onRealTime = useCallback(() => {
+    setChecked(!checked);
+  }, [checked]);
 
   return (
     <Blocks
@@ -40,6 +92,10 @@ const BlocksContainer = () => {
       error={error}
       lastBlockLoading={lastBlockLoading}
       blocksLoading={blocksLoading}
+      id={id}
+      closeTransactions={closeTransactions}
+      onRealTime={onRealTime}
+      checked={checked}
     />
   );
 };
--- src/redux/modules/blocks.js
+++ src/redux/modules/blocks.js
@@ -1,5 +1,5 @@
 import { createAction, handleActions } from 'redux-actions';
-import { takeLatest, put, call } from 'redux-saga/effects';
+import { takeLatest, put, call, select, delay } from 'redux-saga/effects';
 import { createRequestActionTypes } from '../../lib/createRequest';
 import { startLoading, finishLoading } from './loading';
 import web3 from '../../lib/web3API';
@@ -12,6 +12,8 @@ const [
   GET_LAST_BLOCK_NUMBER_FAILURE,
 ] = createRequestActionTypes('blocks/GET_LAST_BLOCK_NUMBER');
 
+const SET_LAST_BLOCK_NUMBER = 'blocks/SET_LAST_BLOCK_NUMBER';
+
 const [
   GET_BLOCK_LIST,
   GET_BLOCK_LIST_SUCCESS,
@@ -23,6 +25,29 @@ const [
   GET_BLOCK_BY_ID_SUCCESS,
   GET_BLOCK_BY_ID_FAILURE,
 ] = createRequestActionTypes('blocks/GET_BLOCK_BY_ID');
+
+const [
+  UPDATE_BLOCK_LIST,
+  UPDATE_BLOCK_LIST_SUCCESS,
+  UPDATE_BLOCK_LIST_FAILURE,
+] = createRequestActionTypes('blocks/UPDATE_BLOCK_LIST');
+
+export const getLastBlockNumber = createAction(GET_LAST_BLOCK_NUMBER);
+export const getBlockList = createAction(
+  GET_BLOCK_LIST,
+  ({ startBlock, endBlock }) => ({ startBlock, endBlock }),
+);
+export const getBlockById = createAction(GET_BLOCK_BY_ID, ({ id }) => ({ id }));
+export const setLastBlockNumber = createAction(
+  SET_LAST_BLOCK_NUMBER,
+  ({ lastBlockNumber }) => ({
+    lastBlockNumber,
+  }),
+);
+export const updateBlockList = createAction(
+  UPDATE_BLOCK_LIST,
+  ({ newBlockNumber }) => ({ newBlockNumber }),
+);
 
 const getLastBlockNumberSaga = () => {
   return function* () {
@@ -75,24 +100,28 @@ const getBlockListSaga = () => {
 const getBlockByIdSaga = () => {
   return function* (action) {
     const { id } = action.payload;
+
     try {
       yield put(startLoading(GET_BLOCK_BY_ID));
 
       const block = yield call(web3.eth.getBlock, id, true);
 
-      yield put({
-        type: GET_BLOCK_BY_ID_SUCCESS,
-        payload: {
-          block,
-        },
-      });
+      if (block) {
+        yield put({
+          type: GET_BLOCK_BY_ID_SUCCESS,
+          payload: {
+            block,
+          },
+        });
 
-      const transactionList = block.transactions.filter(
-        transaction => transaction.value > 0 && transaction.to !== null,
-      );
+        const transactionList = block.transactions.filter(
+          transaction => transaction.value > 0 && transaction.to !== null,
+        );
 
-      yield put(setTransactionList({ transactionList }));
+        yield put(setTransactionList({ transactionList }));
+      }
     } catch (e) {
+      console.log(e);
       yield put({
         type: GET_BLOCK_BY_ID_FAILURE,
         payload: e,
@@ -103,17 +132,55 @@ const getBlockByIdSaga = () => {
   };
 };
 
-export const getLastBlockNumber = createAction(GET_LAST_BLOCK_NUMBER);
-export const getBlockList = createAction(
-  GET_BLOCK_LIST,
-  ({ startBlock, endBlock }) => ({ startBlock, endBlock }),
-);
-export const getBlockById = createAction(GET_BLOCK_BY_ID, ({ id }) => ({ id }));
+const updateBlockListSaga = () => {
+  return function* (action) {
+    const { newBlockNumber } = action.payload;
+    try {
+      yield put(startLoading(UPDATE_BLOCK_LIST));
+
+      const { blockList } = yield select(state => state.blocks);
+
+      yield delay(2000);
+
+      if (blockList) {
+        const latestBlockList = yield call(
+          fetchBlocks,
+          blockList[0].number + 1,
+          newBlockNumber,
+        );
+
+        console.log(latestBlockList);
+
+        const newBlockList = blockList
+          .reverse()
+          .concat(latestBlockList)
+          .reverse();
+        console.log('newBlockList: ', newBlockList);
+
+        yield put({
+          type: UPDATE_BLOCK_LIST_SUCCESS,
+          payload: {
+            newBlockList,
+          },
+        });
+      }
+    } catch (e) {
+      console.log(e);
+      yield put({
+        type: UPDATE_BLOCK_LIST_FAILURE,
+        payload: e,
+        error: true,
+      });
+    }
+    yield put(finishLoading(UPDATE_BLOCK_LIST));
+  };
+};
 
 export function* blocksSaga() {
   yield takeLatest(GET_LAST_BLOCK_NUMBER, getLastBlockNumberSaga());
   yield takeLatest(GET_BLOCK_LIST, getBlockListSaga());
   yield takeLatest(GET_BLOCK_BY_ID, getBlockByIdSaga());
+  yield takeLatest(UPDATE_BLOCK_LIST, updateBlockListSaga());
 }
 
 const initialState = {
@@ -122,6 +189,8 @@ const initialState = {
   block: null,
   transactionList: [],
   error: null,
+  realtime: true,
+  newBlockNumber: null,
 };
 
 const blocks = handleActions(
@@ -150,6 +219,18 @@ const blocks = handleActions(
       block,
     }),
     [GET_BLOCK_BY_ID_FAILURE]: (state, { payload: e }) => ({
+      ...state,
+      error: e,
+    }),
+    [SET_LAST_BLOCK_NUMBER]: (state, { payload: { lastBlockNumber } }) => ({
+      ...state,
+      lastBlockNumber,
+    }),
+    [UPDATE_BLOCK_LIST_SUCCESS]: (state, { payload: { newBlockList } }) => ({
+      ...state,
+      blockList: newBlockList,
+    }),
+    [UPDATE_BLOCK_LIST_FAILURE]: (state, { payload: e }) => ({
       ...state,
       error: e,
     }),
